1d6928b67be8d3974fd2666b696d204880370d46
Jack <406151651@qq.com>
2022-09-07 11:57:57 +0800

    修改项目代码。

--- document/sql/创建数据库.sql
+++ document/sql/创建数据库.sql
@@ -1,12 +1,12 @@
 -- 创建用户
-create user 'mp'@'%' identified by 'XueGod!@#123';
+create user 'mp'@'%' identified by '123456';
 -- 创建数据库
 create database if not exists mp default character set utf8 default collate utf8_general_ci;
 -- 授权mysql5.7
-grant all on mp.* to 'mp'@'%' identified by 'XueGod!@#123';
+grant all on mp.* to 'mp'@'%' identified by '123456';
 -- 授权mysql8.1
 grant all privileges on mp.* to 'mp'@'%' with grant option;
-alter user 'mp'@'%' identified with mysql_native_password by 'XueGod!@#123'; #修改加密规则
-alter user 'mp'@'%' identified by 'XueGod!@#123' password expire never; #更新一下用户的密码且不过期
+alter user 'mp'@'%' identified with mysql_native_password by '123456'; #修改加密规则
+alter user 'mp'@'%' identified by '123456' password expire never; #更新一下用户的密码且不过期
 -- 刷新权限
 flush privileges;
